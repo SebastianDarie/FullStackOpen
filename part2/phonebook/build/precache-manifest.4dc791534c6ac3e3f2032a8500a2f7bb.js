c79301f7b0bc0d996ee8b9fa234a5f409a2330ab
SebastianDarie <s.d.01.2002@gmail.com>
2020-09-29 23:26:09 +0300

    make it better

---
+++ part2/phonebook/build/precache-manifest.4dc791534c6ac3e3f2032a8500a2f7bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98d1647d2857023dbe0cb78cfa2820a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3b721567ad0d4e5a18e",
+    "url": "/static/css/main.7449d2c2.chunk.css"
+  },
+  {
+    "revision": "b692da66e25e6e6cc285",
+    "url": "/static/js/2.11a281b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.11a281b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3b721567ad0d4e5a18e",
+    "url": "/static/js/main.51bc15d0.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
