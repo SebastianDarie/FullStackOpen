d84435edb6ed21a97470a01738b2b1ed9db16385
SebastianDarie <s.d.01.2002@gmail.com>
2020-10-11 21:17:29 +0300

    ex 4.13-4.14

--- part4/blog list/controllers/blogs.js
+++ part4/blog list/controllers/blogs.js
@@ -25,4 +25,25 @@ blogRouter.post('/', async (req, res) => {
 	res.status(201).json(savedBlog)
 })
 
+blogRouter.put('/:id', async (req, res) => {
+	const body = req.body
+
+	const blog = {
+		title: body.title,
+		author: body.author,
+		url: body.url,
+		likes: body.likes,
+	}
+
+	const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, blog, {
+		new: true,
+	})
+	res.json(updatedBlog)
+})
+
+blogRouter.delete('/:id', async (req, res) => {
+	await Blog.findByIdAndRemove(req.params.id)
+	res.status(204).end()
+})
+
 module.exports = blogRouter
--- part4/blog list/tests/blog_api.test.js
+++ part4/blog list/tests/blog_api.test.js
@@ -15,65 +15,106 @@ beforeEach(async () => {
 	}
 })
 
-test('notes are returned as json', async () => {
-	await api
-		.get('/api/blogs')
-		.expect(200)
-		.expect('Content-Type', /application\/json/)
+describe('when there are initial blogs', () => {
+	test('notes are returned as json', async () => {
+		await api
+			.get('/api/blogs')
+			.expect(200)
+			.expect('Content-Type', /application\/json/)
+	})
+
+	test('all blogs are returned', async () => {
+		const response = await api.get('/api/blogs')
+
+		expect(response.body.length).toBe(helper.blogList.length)
+	})
+
+	test('verify unique id field', async () => {
+		const response = await api.get('/api/blogs')
+
+		expect(response.body[0].id).toBeDefined()
+	})
 })
 
-test('all blogs are returned', async () => {
-	const response = await api.get('/api/blogs')
+describe('post blogs and validate', () => {
+	test('can post blog to db', async () => {
+		const newBlog = {
+			title: 'Test Blog',
+			author: 'Fullstack Dev',
+			url: 'www.mernlabs.team',
+			likes: 9,
+		}
 
-	expect(response.body.length).toBe(helper.blogList.length)
+		await api.post('/api/blogs').expect(201).send(newBlog)
+
+		const changedBlogs = await helper.blogsInDB()
+
+		expect(changedBlogs).toHaveLength(helper.blogList.length + 1)
+	})
+
+	test('likes set default to 0', async () => {
+		const newBlog = {
+			title: 'Test Blog 2',
+			author: 'Frontend Dev',
+			url: 'www.mernlabs.io',
+		}
+
+		await api.post('/api/blogs').expect(201).send(newBlog)
+
+		const addedBlog = await Blog.find({ title: 'Test Blog 2' })
+
+		expect(addedBlog[0].likes).toEqual(0)
+	})
+
+	test('return bad request if not title and url', async () => {
+		const newBlog = {
+			author: 'Noob Dev',
+			likes: 9,
+		}
+
+		await api.post('/api/blogs').send(newBlog).expect(400)
+
+		const blogs = await helper.blogsInDB()
+
+		expect(blogs).toHaveLength(helper.blogList.length)
+	})
 })
 
-test('verify unique id field', async () => {
-	const response = await api.get('/api/blogs')
+describe('deletion of a blog', () => {
+	test('succeeds with status code 204 if id is valid', async () => {
+		const blogsAtStart = await helper.blogsInDB()
+		const blogToDelete = blogsAtStart[0]
 
-	expect(response.body[0].id).toBeDefined()
+		await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+
+		const blogsAtEnd = await helper.blogsInDB()
+
+		expect(blogsAtEnd).toHaveLength(helper.blogList.length - 1)
+
+		expect(blogsAtEnd).not.toContain(blogToDelete)
+	})
 })
 
-test('can post blog to db', async () => {
-	const newBlog = {
-		title: 'Test Blog',
-		author: 'Fullstack Dev',
-		url: 'www.mernlabs.team',
-		likes: 9,
-	}
+describe('update of a blog', () => {
+	test('succeeds if id is valid', async () => {
+		const blogsAtStart = await helper.blogsInDB()
+		const blogToUpdate = blogsAtStart[0]
 
-	await api.post('/api/blogs').expect(201).send(newBlog)
+		const changes = {
+			title: 'Test Blog',
+			author: 'Fullstack Dev',
+			url: 'www.mernlabs.io',
+			likes: 9,
+		}
 
-	const changedBlogs = await helper.blogsInDB()
+		const updatedBlog = await api.put(`/api/blogs/${blogToUpdate.id}`, changes)
 
-	expect(changedBlogs).toHaveLength(helper.blogList.length + 1)
-})
+		const blogsAtEnd = await helper.blogsInDB()
 
-test('likes set default to 0', async () => {
-	const newBlog = {
-		title: 'Test Blog 2',
-		author: 'Frontend Dev',
-		url: 'www.mernlabs.io',
-	}
+		expect(blogsAtEnd).toHaveLength(helper.blogList.length)
 
-	await api.post('/api/blogs').expect(201).send(newBlog)
-
-	const addedBlog = await Blog.find({ title: 'Test Blog 2' })
-
-	expect(addedBlog[0].likes).toEqual(0)
-})
-
-test('return bad request if not title and url', async () => {
-	const newBlog = {
-		author: 'Noob Dev',
-		likes: 9,
-	}
-
-	await api.post('/api/blogs').send(newBlog).expect(400)
-
-	const blogs = await helper.blogsInDB()
-
-	expect(blogs).toHaveLength(helper.blogList.length)
+		expect(updatedBlog).not.toEqual(blogToUpdate)
+	})
 })
 
 //npm test -- tests/blog_api.test.js
